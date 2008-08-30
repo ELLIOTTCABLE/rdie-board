58dd1b38668f1d0e5cb319f0450a7d6989f70c76
elliottcable <git@elliottcable.name>
2008-08-29 22:40:35 -0800

    Fixed a bug in the tile server, and renamed all the attributes to match what the Javascript expects

--- tile_server/api.rb
+++ tile_server/api.rb
@@ -21,40 +21,40 @@ class Tile
   
   Tiles = []
   attr_reader :name
-  attr_accessor :style # :style
+  attr_accessor :css
   # Custom defined, due to the fact we need to push the name in front of each
   # attribute
-  def style= style
-    if source.respond_to? :to_str
-      style_doc = CssParser::Parser.new
-      style_doc.add_block! style
+  def css= css
+    if css.respond_to? :to_str
+      css_doc = CssParser::Parser.new
+      css_doc.add_block! css
       # Why can't I use #map? )-:
       styles = []
-      style_doc.each_selector do |selectors, declarations, _|
+      css_doc.each_selector do |selectors, declarations, _|
         styles << "#tiles .#{@name}.tile #{selectors} {#{declarations}}"
       end
-      @style = styles.join("\n")
+      @css = styles.join("\n")
     else
-      @style = style
+      @css = css
     end
   end
-  attr_accessor :source # :svg
+  attr_accessor :svg
   # Custom defined, due to the fact we need to validate the presence of the
   # xmlns property.
-  def source= source
-    if source.respond_to? :to_str
-      roots = Hpricot::XML(source.to_s).search('>').reject {|e| e.is_a? Hpricot::Text }
+  def svg= svg
+    if svg.respond_to? :to_str
+      roots = Hpricot::XML(svg.to_s).search('>').reject {|e| e.is_a? Hpricot::Text }
       roots.set(:xmlns => 'http://www.w3.org/2000/svg')
-      @source = roots.to_html
+      @svg = roots.to_html
     else
-      @source = source
+      @svg = svg
     end
   end
   
   def initialize name, opts = {}
     @name = name
-    @style = opts[:style] || opts[:css] || nil
-    @soure = opts[:source] || opts[:svg] || TileBackground
+    @css = opts[:css] || nil
+    @svg = opts[:svg] || TileBackground
     
     yield self if block_given?
     
@@ -64,16 +64,16 @@ class Tile
   def to_h
     {
       :id => Tiles.index(self),
-      :name => name,
-      :css  => style,
-      :svg  => source
+      :name => @name,
+      :css  => @css,
+      :svg  => @svg
     }
   end
 end
 
 Tile.new :null # Tile of ID 0 should always be an empty tile named 'null'
 Tile.new :test do |tile|
-  tile.style = ".background { fill: #00CC00; }"
+  tile.css = ".background { fill: #00CC00; }"
 end
 
 EmptyMap = []
