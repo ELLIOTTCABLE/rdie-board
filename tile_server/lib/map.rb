682c34bb061cf19d9f88f26bf4131334d26e9fc6
elliottcable <git@elliottcable.name>
2008-09-04 16:47:45 -0800

    Using slightly larger maps across the board, and added a ridiculously large 250x250 one.

--- tile_server/lib/map.rb
+++ tile_server/lib/map.rb
@@ -22,4 +22,5 @@ Map.new [[0, 0], [0, 0]] # Map of ID 0 should always be an empty 2x2 map of 'nul
 Map.new( Array.new(10){|| Array.new(10, [1]) } ) # Empty test map, 10x10
 Map.new( Array.new(10){|| Array.new(10, [2]) } ) # Empty grass map, 10x10
 Map.new( Array.new(25){|| Array.new(25, [2]) } ) # Empty grass map, 25x25
-Map.new( Array.new(75){|| Array.new(75, [2]) } ) # Empty grass map, 75x75
+Map.new( Array.new(100){|| Array.new(100, [2]) } ) # Empty grass map, 100x100
+Map.new( Array.new(250){|| Array.new(250, [2]) } ) # Empty grass map, 250x250
